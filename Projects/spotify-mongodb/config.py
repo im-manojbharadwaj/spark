dada36a4013654b684566260ed24f3ff7862a776
Manoj Chandrashekar <manoj.7892194285@gmail.com>
2022-03-04 15:35:14 +0530

    pushing spotify-mongodb project

---
+++ Projects/spotify-mongodb/config.py
@@ -0,0 +1,3 @@
+USER_ID = '31x324qx7jljp3mc6bok27jm6kca'
+TOKEN = 'BQAcO7pTXcizYGNhvE_jYT9t90MHVLQ9KRkL-fdfQ4rcWsmmjdXJt8vF8aR18Pzs8YF8lJRE8PHWtpw3FKfEg5kGwI29tka-UktPWTYF097hKPBzFRp4g-ExPpKwMLg32Axz_fnpuribD6s9slcx7e01fbXg4JEJsSPdXfvs'
+pw = 'unlock'
